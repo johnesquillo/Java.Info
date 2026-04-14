b0ac064e99abcbf9be3aeba4d3c56c7677d296d4
johnesquillo <johnesquillo286@gmail.com>
2026-04-14 20:48:32 +0800

    Initial commit

--- ConditionalOperator.js
+++ ConditionalOperator.js
@@ -1,12 +1,1 @@
-let num = prompt('Enter a number (1-10)');
-
-if (num > 0) {
-    alert(`${num}`);
-} else if (num < 0) {
-    alert(`${num}`);
-} else if (num == 0) {
-    alert(`${num}`)
-
-} else {
-    alert(`${num}`);
-}
+let result = (a + b < 4) ? 'Below' : 'Over';
