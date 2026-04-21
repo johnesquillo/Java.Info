fcb6a92f0a17de9223d7af3867999bb2c0b7787d
johnesquillo <johnesquillo286@gmail.com>
2026-04-21 21:28:12 +0800

    Initial commit

--- LogicalOperator.js
+++ LogicalOperator.js
@@ -1,22 +1,2 @@
-let username = prompt('Enter your username', '');
-
-if (username === 'Admin') {
-
-    let password = prompt('Enter your password', '');
-
-    if (password === 'John') {
-        alert('Welcome!');
-
-    }else if (password !== 'John' || password === null) {
-        alert('Cancelled');
-
-    }else {
-        alert('Wrong password');
-    }
-
-}else if (username !== 'Admin' || username === null) {
-    alert('Cancelled');
-
-}else {
-    alert('Who are you?'); 
-}
+let name = "john" 
+alert(name ?? 'Walang laman');
