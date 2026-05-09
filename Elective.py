38c9f35f9306b38f01cc10c0c357e8e4bc752fd3
johnesquillo <johnesquillo286@gmail.com>
2026-05-09 22:38:50 +0800

    Initial commit

--- Elective.py
+++ Elective.py
@@ -2,5 +2,5 @@ student = dict(name="John", age=21)
 
 student.update({"name": "Josh", "Gender": "Male", "age": 25, "address": "Muntinlupa City"} )
 
-for key in student:
-    print(key)
+for key, value in student.items():
+    print(key, value)
