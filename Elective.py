e00287673f3145eba24bab20f3019a464557d197
johnesquillo <johnesquillo286@gmail.com>
2026-05-10 16:41:44 +0800

    Initial commit

--- Elective.py
+++ Elective.py
@@ -1,5 +1,7 @@
 import numpy as np
-arr2D = np.array([[1, 2],[3, 4]])
 
-result = arr2D[1, 0]
-print(result)
+arr = np.array([1, 2, 3, 4, 5])
+
+print(np.mean(arr))
+print(np.sum(arr))
+print(np.max(arr))
