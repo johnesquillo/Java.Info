62bf418f251bd5a2d26812d943af04542f5947eb
johnesquillo <johnesquillo286@gmail.com>
2026-05-19 23:01:16 +0800

    Initial commit

--- function.js
+++ function.js
@@ -1,13 +1,5 @@
-function checkAge(age) {
-    if (age >= 18) {
-        return true;
-    }else {
-        return confirm('Do you have a permission of your parents?');
-    }
+function doNothing() {
+    alert(doNothing() == undefined);
 }
-let age = prompt('How old are you?', 18);
-if ( checkAge(age)) {
-    alert('Access granted');
-} else {
-    alert('Access denied');
-}
+
+doNothing();
