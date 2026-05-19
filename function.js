4222b0b17d98dee229269a909803b36e202efb7c
johnesquillo <johnesquillo286@gmail.com>
2026-05-20 00:13:24 +0800

    Initial commit

--- function.js
+++ function.js
@@ -1,8 +1,14 @@
 function showPrimes(n) {
-    nextPrime: for (let i = 2; i < n; i++) {
-        for (let j = 2; j < i; j++) {
-            if (i % j == 0) continue nextPrime;
-        }
-            alert(i);
+    for (let i = 2; i < n; i++) {
+        if(!isPrime(i)) continue;
+
+        alert(i);
     }
+}
+
+function isPrime(n) {
+    for (let i = 2; i < n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
 }
