16f994aad66a8390641a1f3a6dfd34bd378dc891
johnesquillo <johnesquillo286@gmail.com>
2026-05-20 19:05:41 +0800

    Initial commit

--- function.js
+++ function.js
@@ -1,8 +1,17 @@
-function min(a, b) {
-  if (a < b) {
-    return a;
-  } else {
-    return b;
-  }
+function getPow(x, n) {
+    let num = x;
+
+    for(let i = 1; i < n; i++) {
+        num *= x;
+    }
+    return num;
 }
 
+let x = prompt('x?');
+let n = prompt('n?');
+
+if (n < 1) {
+    alert(`Power ${n} is not supported, use a positive integer`)
+}else{
+    alert(getPow(x, n));
+}
