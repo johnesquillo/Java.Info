16029b412b84c5c42425144b6c5eb575061cbdee
johnesquillo <johnesquillo286@gmail.com>
2026-05-24 16:54:34 +0800

    Initial commit

--- function.js
+++ function.js
@@ -1,8 +1,4 @@
 function showMessage(from, text = 'Hello') {
-    from = '*' + from + '*';
     alert(from + ':' + text);
 }
-
-let from = 'Ann';
-showMessage(from);
-alert(from);
+showMessage('Ann');
