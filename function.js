58938c273bec007d62e2fffb1c2c2933e6c26521
johnesquillo <johnesquillo286@gmail.com>
2026-05-27 16:49:50 +0800

    Initial commit

--- function.js
+++ function.js
@@ -1,16 +1,5 @@
-function pow(x, n) {
-    let num = x;
-    for (let i = 1; i < n; i++){
-        num *= x;
-    }
-        return num;
+function showMessage() {
+    alert("Access granted!");
 }
 
-let x = prompt('x?');
-let n = prompt('n?');
-
-if (n < 1) {
-    alert(`Power ${n} is not supported, use a positive integer`)
-} else {
-    alert(pow(x, n));
-}
+showMessage();
