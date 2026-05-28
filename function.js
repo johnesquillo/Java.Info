b0df8d464ba45f7a926ebe5092d1db4fc6be3808
johnesquillo <johnesquillo286@gmail.com>
2026-05-28 20:45:48 +0800

    Initial commit

--- function.js
+++ function.js
@@ -1,17 +1,7 @@
 let age = prompt('How old are you?', 18);
 
-let welcome;
-
-if (age < 18) {
-
-    welcome = function() {
-        alert('Hello!');
-    };
-
-}else {
-    welcome = function() {
-        alert('Greeting!');
-    };
-}
+let welcome = (age < 18) ? 
+function() {alert('Hello!'); } :
+function() {alert('Greetings!'); };
 
 welcome();
