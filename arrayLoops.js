95d291da6ca1bc5cdbd1b4d3acf5a2d2fa2e0798
johnesquillo <johnesquillo286@gmail.com>
2026-07-12 16:53:59 +0800

    Initial commit

--- arrayLoops.js
+++ arrayLoops.js
@@ -1,9 +1,9 @@
-function toUpper(string) {
-    return string.toUpperCase();
+function lCat(cat) {
+    return cat.startsWith("L");
 }
 
 const cats = ['Leopard', 'Serval', 'Jaguar', 'Tiger', 'Caracal', 'Lion'];
 
-const upperCats = cats.map(toUpper);
+const filtered = cats.filter(lCat);
 
-alert(upperCats);
+alert(filtered);
