4a3311745731d9353ec578c57c13fccc339e95c0
johnesquillo <johnesquillo286@gmail.com>
2026-07-19 00:40:48 +0800

    My second solution

--- arrayLoops.js
+++ arrayLoops.js
@@ -3,6 +3,8 @@ function getMaxSubSum(arr) {
     
 }
 
-const numbers = [-1, 2, 3, -9];
+const numbers = [ 
+    [-1, 2, 3, -9], 
+]
 
-alert(numbers[1] + numbers[2]);
+alert(numbers[0][1] + numbers[0][2]);
