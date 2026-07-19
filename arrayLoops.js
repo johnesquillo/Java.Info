35a191e8fe14811d22627091f69b2a0288b4ec4c
johnesquillo <johnesquillo286@gmail.com>
2026-07-19 13:27:49 +0800

    Initial commit

--- arrayLoops.js
+++ arrayLoops.js
@@ -6,6 +6,11 @@ function getMaxSubSum(arr) {
 
 const numbers = [ 
     [-1, 2, 3, -9], 
-]
+    [2, -1, 2, 3, -9],
+    [-1, 2, 3, -9, 11],
+    [-2, -1, 1, 2],
+    [100, -9, 2, -3, 5],
+    [1, 2, 3]
+];
 
 alert(numbers[0][1] + numbers[0][2]);
