9f0383e11e435cf28a32fd261638bbaa37f675bc
johnesquillo <johnesquillo286@gmail.com>
2026-07-20 09:48:47 +0800

    Solution from JS.info

--- arrayLoops.js
+++ arrayLoops.js
@@ -1,16 +1,31 @@
 // JS.Info Task
 function getMaxSubSum(arr) {
-   
-    
+
+    let maxSum = 0;
+    let partialSum = 0;
+
+    for (let item of arr) {
+        partialSum += item;
+        maxSum = Math.max(maxSum, partialSum);
+        if (partialSum < 0) partialSum = 0;
+    }
+        return maxSum;
+
 }
 
-const numbers = [ 
-    [-1, 2, 3, -9], 
-    [2, -1, 2, 3, -9],
-    [-1, 2, 3, -9, 11],
-    [-2, -1, 1, 2],
-    [100, -9, 2, -3, 5],
-    [1, 2, 3]
-];
+alert( getMaxSubSum([-1, 2, 3, -9]) ); 
+alert( getMaxSubSum([-1, 2, 3, -9, 11]) );
+alert( getMaxSubSum([-2, -1, 1, 2]) ); 
+alert( getMaxSubSum([100, -9, 2, -3, 5]) ); 
+alert( getMaxSubSum([1, 2, 3]) ); 
+alert( getMaxSubSum([-1, -2, -3]) ); 
 
-alert(numbers[0][1] + numbers[0][2]);
+
+
+
+       
+    
+      
+
+
+
