9af0dcf5b8c1b91e29cea7d43302e14d3eec9f28
johnesquillo <johnesquillo286@gmail.com>
2026-07-20 19:42:57 +0800

    initial commit

--- arrayLoops.js
+++ arrayLoops.js
@@ -4,10 +4,10 @@ function getMaxSubSum(arr) {
     let maxSum = 0;
     let partialSum = 0;
 
-    for (let item of arr) {
-        partialSum += item;
-        maxSum = Math.max(maxSum, partialSum);
-        if (partialSum < 0) partialSum = 0;
+    for (let items of arr) {
+        partialSum += items;
+        maxSum = Math.max(partialSum, maxSum);
+        if (partialSum < 0) partialSum = 0
     }
         return maxSum;
 
