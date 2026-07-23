834cc83c7960229d6e4b76d90927a772cd8e911f
johnesquillo <johnesquillo286@gmail.com>
2026-07-23 22:51:56 +0800

    Initial commit

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,6 +1,8 @@
-const arr = ["John", "Esquillo", "Malones"];
+const arr = ["John", "Esquillo", "Malones", "John"];
 
+alert( arr.includes("John"));
 alert( arr.indexOf("John"));
+alert( arr.lastIndexOf("John"));
 
 let arrays = arr.forEach((item, index, array) => {
     alert(`${item} is at index ${index} in ${array}`);
