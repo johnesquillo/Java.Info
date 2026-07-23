b7c0b31440de63e6868098fd1f92e2ab4424db9d
johnesquillo <johnesquillo286@gmail.com>
2026-07-23 23:30:10 +0800

    Initial commit

--- arrayMethods.js
+++ arrayMethods.js
@@ -7,3 +7,14 @@ alert( arr.lastIndexOf("John"));
 let arrays = arr.forEach((item, index, array) => {
     alert(`${item} is at index ${index} in ${array}`);
 });
+
+
+let users = [
+    {id: 1, name: "John"},
+    {id: 2, name: "Jake"},
+    {id: 3, name: "Josh"}
+];
+
+let user = users.find(item => item.id == 1);
+
+alert( user.name );
