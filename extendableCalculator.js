428ab1f073a902e32d6375c9d29dd532c3baa51f
johnesquillo <johnesquillo286@gmail.com>
2026-08-05 16:48:55 +0800

    Initial commit

--- extendableCalculator.js
+++ extendableCalculator.js
@@ -1,3 +1,17 @@
 function calculate(str) {
-    
+    return str.split('').reduce((acc, char) => {
+        if (char === '+') {
+            return acc + 1;
+        }
+        if ( char === '-') {
+            return acc - 1;
+        }
+    }, 0);
 }
+
+addMethod('multiply', (a, b) => a * b);
+addMethod('divide', (a, b) => a / b);
+addMethod('power', (a, b) => Math.pow(a, b));
+addMethod('modulus', (a, b) => a % b);
+addMethod('max', (a, b) => Math.max(a, b));
+alert(calculate('++-+'));
