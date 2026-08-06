6e5b4fc487f8280f85954266c2e959d1a80e9d31
johnesquillo <johnesquillo286@gmail.com>
2026-08-06 16:31:06 +0800

    Done with my solution for this task

--- extendableCalculator.js
+++ extendableCalculator.js
@@ -1,22 +1,22 @@
 function Calculator() {
+    
     this.methods = {
+        "+" : (a, b) => a + b,
         "-" : (a, b) => a - b,
-        "+" : (a, b) => a + b
     };
-
     this.calculate = (str) => {
         const split = str.split(" ");
-        const a = +split[0];
+        const a = split[0];
         const op = split[1];
-        const b = +split[2];
+        const b = split[2];
 
         if (!this.methods[op] || isNaN(a) || isNaN(b)) {
             return NaN;
         }
         return this.methods[op](a, b);
-    };
+    }
     this.addMethod = (name, func) => {
-        this.methods[name] = func;
+        this.methods[name] = func
     };
 }
 
@@ -25,13 +25,16 @@ let calc = new Calculator();
 alert(calc.calculate("3 + 7"));
 alert(calc.calculate("3 - 7"));
 
-let powerCalc = new Calculator();
-
 calc.addMethod("*", (a, b) => a * b);
-calc.addMethod("/", (a, b) => a / b);
-calc.addMethod("**", (a, b) => a ** b);
+alert(calc.calculate("3 * 7"));
 
-alert(calc.calculate("2 * 3"));
-alert(calc.calculate("2 / 3"));
+calc.addMethod("/", (a, b) => a / b);
+alert(calc.calculate("10 / 2"));
+
+calc.addMethod("**", (a, b) => a ** b);
 alert(calc.calculate("2 ** 3"));
 
+let total = calc.calculate("3 + 7") + calc.calculate("10 / 2") + calc.calculate("2 ** 3") + 
+            calc.calculate("3 * 7") + calc.calculate("3 - 7");
+
+            alert(total);
