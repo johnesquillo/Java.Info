07257be25cae5b7f53a87ee4bd58e69fc7cd2c47
johnesquillo <johnesquillo286@gmail.com>
2026-08-10 18:41:50 +0800

    Initial commit

--- shuffleArray.js
+++ shuffleArray.js
@@ -1,6 +1,10 @@
-function shuffle(array) {
-    return array
+function shuffle(arr) {
+    return arr
+    Math.floor(Math.random() * arr.length)
 }
 
 let arr = [1, 2, 3];
+
+alert(shuffle(arr));
+
 
