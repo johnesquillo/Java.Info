35c62697e53ada3a24e27d807a34473322fb6db3
johnesquillo <johnesquillo286@gmail.com>
2026-08-11 16:15:59 +0800

    Done with my solution for this task

--- getAverageAge.js
+++ getAverageAge.js
@@ -1,15 +1,14 @@
 function getAverageAge(users) {
+    
+    let getAge = users.map(user => user.age).reduce((sum, current) => sum + current, 0);
+    return getAge / users.length;
 
-        let getAge = users.map(user => user.age)
-        .reduce((sum, current) => sum + current, 0)
-
-        return getAge / users.length;
 }
 
-let usersInfo = [
+let user = [
     {name: "John", age: 25},
-    {name: "Pete", age: 30},
-    {name: "Mary", age: 29}
-];  
+    {name: "Jake", age: 30},
+    {name: "Josh", age: 29}
+];
 
-alert(getAverageAge(usersInfo));
+alert(getAverageAge(user));
