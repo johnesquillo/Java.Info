6796365323da0de79580dc51c9b7c5fcbb3b0188
johnesquillo <johnesquillo286@gmail.com>
2026-08-15 20:42:09 +0800

    Initial commit

--- spread.js
+++ spread.js
@@ -5,3 +5,29 @@ content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 
 container.appendChild(content);
+
+const redText = document.createElement("p");
+redText.textContent = "Hey I'm red!";
+redText.style.color = "red";
+document.body.appendChild(redText);
+
+const blueText = document.createElement("h3");
+blueText.textContent = "I'm a blue h3!";
+blueText.style.color = "blue";
+document.body.appendChild(blueText);
+
+
+const div = document.createElement("div");
+div.style.border = "1px solid black";
+div.style.backgroundColor = "pink";
+document.body.appendChild(div);
+
+const h1 = document.createElement("h1");
+h1.textContent = "I'm in a div!";
+document.appendChild(h1);
+
+
+const paragraph = document.createElement("p");
+paragraph.textContent = "ME TOO!";
+document.appendChild(paragraph);
+
